b0da8117b6526164cf543841235c70e977517874
umjoji <30466162+umjoji@users.noreply.github.com>
2023-07-12 14:35:16 +0100

    Add missing import

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,12 +4,13 @@ Execute multiple coroutines
 """
 
 import asyncio
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Args:
         n: number of times to execute wait_random()
@@ -20,4 +21,4 @@ async def wait_n(n: int, max_delay: int) -> float:
     # create tasks using list comprehension
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in
              range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]  # type: ignore
+    return [await task for task in asyncio.as_completed(tasks)]
